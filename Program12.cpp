f805084d72469cc8becb5b663ccdd3e42a9d2acb
SK20137 <56750704+SK20137@users.noreply.github.com>
2019-10-20 11:31:14 +0530

    I found bugs in Program12.cpp

--- Program12.cpp
+++ Program12.cpp
@@ -1,13 +1,42 @@
 // Binary to Decimal conversion
 // Complexity= M
 
-int function(int n) { 
+#include <iostream>
+using namespace std;
+
+int b2d(int n) { 
     int temp = n,value = 0, c = 1; 
-    while (temp) { 
-        int r = temp % 10;  
+    int r = 0;
+    while (temp > 10) { 
+        r = temp % 10; 
+        temp = temp/10;
         value += r * c; 
         c *= 2; 
     } 
+    r = temp%10;
+    value += r * c; 
     return value; 
+}
+
+int o2d(int n)
+{
+    
+}
+
+int main() {
+	//code
+	int n = 01100;
+	if(n%10 != 0 && n%10 != 1)
+	{
+	    cout<<"oct";
+	    int res = o2d(n);
+	}
+	else
+	{
+	    cout<<"binary";
+	    int res = b2d(n);
+	}
+	cout<<res;
+	return 0;
 }
 
